1821771d451351c5d63f38e484e2cc8366ab3763
Jacob Webb rpi <jacob.webb22@gmail.com>
2015-03-22 21:05:17 -0400

    nixie with counter working

--- nixie.c
+++ nixie.c
@@ -2,25 +2,32 @@
 #include <wiringPiSPI.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-int channel = 0;
-int speed = 4000000;
+#include <time.h>
 
 int main (void)
 {
+	int speed = 4000000;
+	int channel = 0;
+
+	unsigned char one = 0;
+	unsigned char two = 0;
+	unsigned char Text = 0;
+
 	wiringPiSetup () ;
 	wiringPiSPISetup (channel, speed) ;
 
-	unsigned char one = 1;
-    unsigned char two = 2;
-    one = one << 4;
-    unsigned char Text = one + two;
-
-    int i = 0;
-	for (i = 1; i <= 10000000; i++)
+	int i = 0;
+	for (i = 1; i <= 5; i++)
 	{
-	wiringPiSPIDataRW (channel, &Text, sizeof(Text)) ;
-	printf( "%d", i);
+
+	one = i%10;
+	two = i/10;
+	one = one << 4;
+	Text = one + two;
+
+	wiringPiSPIDataRW (channel, &Text, 1) ;
+	printf( "%d\n", i);
+	delay(1000);
 	}
 
 	return 0 ;
