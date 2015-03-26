3fee1600b407515781e8fcc9aa5395fe2000cbec
Pi <pi@pi.(none)>
2015-03-26 01:33:40 +0000

    ported to A+ and added more nixies

--- nixie.py
+++ nixie.py
@@ -7,12 +7,17 @@ import datetime
 spi = spidev.SpiDev()
 spi.open(0,0)
 
-for i in range(1,100):
-	one=datetime.datetime.now().second%10
-	two=datetime.datetime.now().second/10
+for i in range(1,200):
+	current = datetime.datetime.now()
+	one=current.second%10
+	two=current.second/10
 	one = one<<4
-	Text = one + two
-	spi.xfer2([Text])
+	seconds = one + two
+	one=current.minute%10
+        two=current.minute/10
+        one = one<<4
+        minutes = one + two
+	spi.writebytes([seconds, minutes])
 	time.sleep(0.1)   # sleep in seconds
 # end for
 
